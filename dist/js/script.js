71021bf4b105bcc44816b5beafe4d865ee6b5100
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2020-07-26 15:30:07 +0200

    Working on beauty section
    trying add counter to numbers

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,3 +4,41 @@ $(document).ready(function(){
         // $('.mobile-menu__nav').slideToggle();
     })
 });
+
+document.ready(function($){
+    $('.workers')
+        
+})
+
+
+
+
+
+
+
+
+
+
+// var check = 1;
+// var target = $('.years, .customers, .workers'); // You counter class
+// var targetPos = target.offset().top;
+// var winHeight = $(window).height();
+// var scrollToElem = targetPos - winHeight;
+
+// $(window).scroll(function() {
+//   var winScrollTop = $(this).scrollTop();
+//   if(winScrollTop > scrollToElem && check) {
+//     $('.years, .customers, .workers').each(function() {
+//       $(this).prop('Counter', -1).animate({
+//         Counter: $(this).text()
+//       },{
+//         duration: 3000,
+//         easing: 'swing',
+//         step: function(now) {
+//           $(this).text(Math.ceil(now));
+//         }
+//       });
+//     });
+//     check = 0;
+//   }
+// });
